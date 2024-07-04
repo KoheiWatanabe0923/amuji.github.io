d1f86160137a90d0405e0c977d4c69ab449f6c65
KoheiWatanabe0923 <129282878+KoheiWatanabe0923@users.noreply.github.com>
2024-07-04 10:05:50 +0900

    first_commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="ja">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] ."/inc/inc-head.php"); ?>
+    <body id="home">
+        <div id="wrapper">    
+            <?php include( $_SERVER['DOCUMENT_ROOT'] ."/inc/inc-header.php"); ?>
+            <div id="main_area">
+                <div id="first_view">
+                    <div class="fv_slide">
+                        <div class="fv_bg"></div>
+                    </div><!--fv_slide -->
+                    <div class="fv_logo">
+                        <img src="/img/fv_logo.png" alt="佐渡の南インドカレー亜夢路" width="442" hight="266">
+                    </div><!--fv_logo -->
+                </div><!-- / #first_view -->
+                <section id="news" class="home_section">
+                    <div class="main_width">
+                        <div class="section_title">
+                            <h2>NEWS</h2>
+                        </div>
+                        <div class="news_group">
+                            <a href="/news/detail/" class="news_block">
+                                <p class="date">2021.12.24</p>
+                                <p class="news_title">00月00日（◯）〜00月00日（◯）、カレーマルシェ佐渡0000年度フェスタに出店します。新作カレーもご用意しています</p>
+                            </a>
+                            <a href="/news/detail/" class="news_block">
+                                <p class="date">2021.12.24</p>
+                                <p class="news_title">00月00日（◯）〜00月00日（◯）、カレーマルシェ佐渡0000年度フェスタに出店します。新作カレーもご用意しています</p>
+                            </a>
+                            <a href="/news/detail/" class="news_block">
+                                <p class="date">2021.12.24</p>
+                                <p class="news_title">00月00日（◯）〜00月00日（◯）、カレーマルシェ佐渡0000年度フェスタに出店します。新作カレーもご用意しています</p>
+                            </a>
+                            <a href="/news/detail/" class="news_block">
+                                <p class="date">2021.12.24</p>
+                                <p class="news_title">00月00日（◯）〜00月00日（◯）、カレーマルシェ佐渡0000年度フェスタに出店します。新作カレーもご用意しています</p>
+                            </a>
+                            <a href="/news/detail/" class="news_block">
+                                <p class="date">2021.12.24</p>
+                                <p class="news_title">00月00日（◯）〜00月00日（◯）、カレーマルシェ佐渡0000年度フェスタに出店します。新作カレーもご用意しています</p>
+                            </a>
+                        </div>
+                        <div class="more_btn">
+                            <a href="/news/"><span>MORE</span></a>
+                        </div>
+                    </div><!-- / .main_width -->
+                </section><!-- / #news -->
+                <section id="menu" class="home_section">
+                    <div class="main_width">
+                        <div class="section_title">
+                            <h2>MENU</h2>
+                        </div>
+                        <div class="menu_group">
+                            <div class="menu_block">
+                                <img src="/img/menu01.png" alt="ミールス亜" width="800" hight="800">
+                                <p class="menu_title">ミールス亜</p>
+                                <p class="menu_price">1,000 yen</p>
+                            </div>
+                            <div class="menu_block">
+                                <img src="/img/menu04.png" alt="ミールス夢" width="800" hight="800">
+                                <p class="menu_title">ミールス夢</p>
+                                <p class="menu_price">1,000 yen</p>
+                            </div>
+                            <div class="menu_block">
+                                <img src="/img/menu07.png" alt="ミールス路" width="800" hight="800">
+                                <p class="menu_title">ミールス路</p>
+                                <p class="menu_price">1,000 yen</p>
+                            </div>
+                        </div>
+                        <div class="more_btn">
+                            <a href="/news/"><span>MORE</span></a>
+                        </div>
+                    </div><!-- / .main_width -->
+                </section><!-- / #menu -->
+                <section id="access" class="home_section">
+                    <div class="main_width">
+                        <div class="section_title">
+                            <h2>ACCESS</h2>
+                        </div>
+                        <div class="access_flex">
+                            <div class="access_map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3167.277356488669!2d140.55526337586852!3d37.454172822067896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60206686f93a3683%3A0x50c6fd005344d435!2z5LiA6Iis56S-5Zuj5rOV5Lq6U3dpdGNo!5e0!3m2!1sja!2sjp!4v1719994808870!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                            <div class="access_prof">
+                                <dl>
+                                    <dt>佐渡の南インドカレー<br>亜夢路（AMUJI）</dt>
+                                    <dd></dd>
+                                </dl>
+                                <dl>
+                                    <dt>営業時間</dt>
+                                    <dd>営業時間<br>
+                                    10:30～16:00（不定休）<br>
+                                    営業日は当Webサイトのお知らせや、SNSをご確認ください。</dd>
+                                </dl>
+                                <dl>
+                                    <dt>店内</dt>
+                                    <dd>カウンター×5席、2人テーブル×4席<br>
+                                    お子様歓迎、ペット不可</dd>
+                                </dl>
+                                <dl>
+                                    <dt>住所</dt>
+                                    <dd>〒952-1211 新潟県佐渡市中興乙1427</dd>
+                                </dl>
+                                <dl>
+                                    <dt>電話</dt>
+                                    <dd>01-2345-6789</dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </div><!-- / .main_width -->
+                </section><!-- / #access -->
+            </div><!-- / #main_area -->
+            <?php include( $_SERVER['DOCUMENT_ROOT'] ."/inc/inc-footer.php"); ?>
+        </div><!--wrapper-->
+        <?php include( $_SERVER['DOCUMENT_ROOT'] ."/inc/inc-sp_navi.php"); ?>
+    </body>
+</html>
